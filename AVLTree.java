767a481703fdacfd2ec1111a1c29ec7f839aa066
Sofía Velásquez <98661058+Sofiamishel2003@users.noreply.github.com>
2023-04-20 18:21:04 -0600

    Funcional AVLTree

---
+++ AVLTree.java
@@ -0,0 +1,143 @@
+import java.util.ArrayList;
+
+public class AVLTree {
+    private Node root;
+
+    private class Node {
+        String key;
+        ArrayList<String> value;
+        Node left, right;
+        int height;
+
+        public Node(String key, ArrayList<String> value) {
+            this.key = key;
+            this.value = value;
+            this.height = 1;
+        }
+    }
+
+    public void insert(String key, ArrayList<String> value) {
+        root = insert(root, key, value);
+    }
+
+    private Node insert(Node node, String key, ArrayList<String> value) {
+        if (node == null) {
+            return new Node(key, value);
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            node.left = insert(node.left, key, value);
+        } else if (cmp > 0) {
+            node.right = insert(node.right, key, value);
+        } else {
+            // Elemento repetido, no se permite
+            return node;
+        }
+
+        node.height = 1 + Math.max(height(node.left), height(node.right));
+
+        int balance = getBalance(node);
+
+        // Caso 1: rotación a la derecha
+        if (balance > 1 && key.compareTo(node.left.key) < 0) {
+            return rightRotate(node);
+        }
+
+        // Caso 2: rotación a la izquierda
+        if (balance < -1 && key.compareTo(node.right.key) > 0) {
+            return leftRotate(node);
+        }
+
+        // Caso 3: rotación doble a la derecha
+        if (balance > 1 && key.compareTo(node.left.key) > 0) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        // Caso 4: rotación doble a la izquierda
+        if (balance < -1 && key.compareTo(node.right.key) < 0) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+
+        return node;
+    }
+
+    public ArrayList<String> search(String key) {
+        Node node = search(root, key);
+        if (node != null) {
+            return node.value;
+        }
+        return null;
+    }
+
+    private Node search(Node node, String key) {
+        if (node == null) {
+            return null;
+        }
+
+        int cmp = key.compareTo(node.key);
+        if (cmp < 0) {
+            return search(node.left, key);
+        } else if (cmp > 0) {
+            return search(node.right, key);
+        } else {
+            return node;
+        }
+    }
+
+    private int height(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    private int getBalance(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return height(node.left) - height(node.right);
+    }
+
+    private Node rightRotate(Node y) {
+        Node x = y.left;
+        Node t2 = x.right;
+
+        x.right = y;
+        y.left = t2;
+
+        y.height = 1 + Math.max(height(y.left), height(y.right));
+        x.height = 1 + Math.max(height(x.left), height(x.right));
+
+        return x;
+    }
+
+    private Node leftRotate(Node x) {
+        Node y = x.right;
+        Node t2 = y.left;
+        y.left = x;
+        x.right = t2;
+    
+        x.height = 1 + Math.max(height(x.left), height(x.right));
+        y.height = 1 + Math.max(height(y.left), height(y.right));
+    
+        return y;
+    }
+    public boolean containsKey(String key) {
+        Node current = root;
+        while (current != null) {
+            int cmp = key.compareTo(current.key);
+            if (cmp == 0) {
+                return true; // La clave se encontró en el árbol
+            } else if (cmp < 0) {
+                current = current.left;
+            } else {
+                current = current.right;
+            }
+        }
+        return false; // La clave no se encontró en el árbol
+    }
+    
+}
